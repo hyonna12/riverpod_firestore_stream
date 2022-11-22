123a0fe5e5206deeb9a0bca7152a947437ebac73
hyonna12 <hyonna01@gmail.com>
2022-11-22 14:57:28 +0900

    firestore - db에 insert

--- lib/controller/chat_controller.dart
+++ lib/controller/chat_controller.dart
@@ -1,3 +1,28 @@
-// Provider
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_firestore_stream/domain/chat/chat_firestore_repository.dart';
+import 'package:riverpod_firestore_stream/dto/chat/chat_req_dto.dart';
 
-class ChatController {}
+// Provider
+final chatControllerProvider = Provider((ref) {
+  return ChatController(ref);
+});
+
+class ChatController {
+  Ref _ref;
+  ChatController(this._ref);
+
+  void insert(ChatInsertReqDto dto) async {
+    DocumentReference doc = await _ref.read(chatFirestoreRepositoryProvider).insert(dto);
+    // insert하고 응답데이터를 받아야하는데 통신했기때문에 데이터를 바로 받을 수 없어서 future로 받거나
+    // await async 해서 기다렸다가 future가 아닌 데이터로 받음
+    if (doc.id.isEmpty) {
+      // insert 실패시 - alertDialog 띄움
+    } else {
+      // insert 성공시 - 화면 이동
+    }
+
+    print("디버그:${doc.snapshots()}");
+    print("디버그:${doc.id}");
+  }
+}
--- lib/domain/chat/chat_firestore_repository.dart
+++ lib/domain/chat/chat_firestore_repository.dart
@@ -1,6 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_firestore_stream/dto/chat/chat_req_dto.dart';
+
 // Provider - 메모리 띄우기 위해
+final chatFirestoreRepositoryProvider = Provider((ref) {
+  return ChatFireStoreRepository();
+});
 
 class ChatFireStoreRepository {
   // firestore와 통신
+  final db = FirebaseFirestore.instance;
 
+  Future<DocumentReference> insert(ChatInsertReqDto dto) {
+    // 문서로 돌려받음
+    return db.collection("chat").add(dto.toJson());
+    // db에 insert
+  }
 }
