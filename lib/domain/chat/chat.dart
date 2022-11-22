b3ad2198f43c0d95b920de6a3f0bf14a82e2fd1b
hyonna12 <hyonna01@gmail.com>
2022-11-22 17:44:56 +0900

    수정 - id 값 받아주기

--- lib/domain/chat/chat.dart
+++ lib/domain/chat/chat.dart
@@ -14,8 +14,8 @@ class Chat {
       };
 
   // map -> dart (fromJson)
-  factory Chat.fromJson(Map<String, dynamic> json) => Chat(
-        id: json["id"],
+  factory Chat.fromJson(Map<String, dynamic> json, {String? id}) => Chat(
+        id: id ?? json["id"], // 대체연산자 - null이면 json["id"] 값이 들어감
         from: json["from"],
         to: json["to"],
         msg: json["msg"],
--- lib/domain/chat/chat_firestore_repository.dart
+++ lib/domain/chat/chat_firestore_repository.dart
@@ -5,14 +5,17 @@ import 'package:riverpod_firestore_stream/dto/chat/chat_req_dto.dart';
 
 // 리턴 : Stream<List<Chat>> - 계속 수신 받을 수 있음
 // List<Chat>은 한번 주고 끝나는것
-final chatStreamProvider = StreamProvider<List<Chat>>((ref) {
+final chatStreamProvider = StreamProvider.autoDispose<List<Chat>>((ref) {
+  // 리턴된 메모리가 죽을 때
+  ref.onDispose(() {});
+
   // 스트림을 파싱해서 리턴
   final db = FirebaseFirestore.instance; // db에 접근
   // stream provier의 목적은 db.collection("chat").snapshots(); 리턴하는것
   // 파싱을 repository에서 끝내려고
 
   Stream<QuerySnapshot<Map<String, dynamic>>> stream = db.collection("chat").snapshots(); // map타입 스트림 - 사진찍을건데 타입을 모르니까 map 타입
-  return stream.map((snapshot) => snapshot.docs.map((doc) => Chat.fromJson(doc.data())).toList()); // 파싱
+  return stream.map((snapshot) => snapshot.docs.map((doc) => Chat.fromJson(doc.data(), id: doc.id)).toList()); // 파싱
 });
 // stream 안의 map의 string object의 dynamic 타입을 리턴해주는데 화면에서 파싱하기 힘드니까
 // stream 안의 map 타입을 chat 타입으로 바꿔서 화면에서 파싱하지 않아도 되도록 repository 에서 파싱
