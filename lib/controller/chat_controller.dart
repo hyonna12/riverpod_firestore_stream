7b3bc076b7dd49e9f64db28d9943d3add5fca8c5
hyonna12 <hyonna01@gmail.com>
2022-11-22 15:26:38 +0900

    firestore write 완료

--- lib/controller/chat_controller.dart
+++ lib/controller/chat_controller.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_firestore_stream/core/routes_enum.dart';
 import 'package:riverpod_firestore_stream/domain/chat/chat_firestore_repository.dart';
 import 'package:riverpod_firestore_stream/dto/chat/chat_req_dto.dart';
 
@@ -12,17 +14,25 @@ class ChatController {
   Ref _ref;
   ChatController(this._ref);
 
-  void insert(ChatInsertReqDto dto) async {
-    DocumentReference doc = await _ref.read(chatFirestoreRepositoryProvider).insert(dto);
+  void insert(ChatInsertReqDto dto) {
+    Future<DocumentReference> futureDoc = _ref.read(chatFirestoreRepositoryProvider).insert(dto);
     // insert하고 응답데이터를 받아야하는데 통신했기때문에 데이터를 바로 받을 수 없어서 future로 받거나
     // await async 해서 기다렸다가 future가 아닌 데이터로 받음
-    if (doc.id.isEmpty) {
-      // insert 실패시 - alertDialog 띄움
-    } else {
-      // insert 성공시 - 화면 이동
-    }
 
-    print("디버그:${doc.snapshots()}");
-    print("디버그:${doc.id}");
+    futureDoc.then(
+      (value) {
+        print("document id: ${value.id}");
+        // 여기서는 화면이동
+        // Navigator.pushNamed(context, Routes.login.path);
+        // Navigator.popAndPushNamed(context, Routes.login.path); // pop 띄우면서 화면이동
+        // Navigator.pop(context); // 현재 화면 빠져나오기
+        // Navigator.pushNamedAndRemoveUntil(context, Routes.login.path, (route) => false); //새로운 화면 띄우면서 기존 화면 날리기
+      },
+    ).onError(
+      (error, stackTrace) {
+        print("error:${error}");
+        // 여기서는 경고창(에러메시지)
+      },
+    );
   }
 }
